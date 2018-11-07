81149299172cdaf49e007f5fd18a4bacfb8bedc0
grapevine <jinsw2001@naver.com>
2018-11-08 02:49:43 +0900

    test

--- index.js
+++ index.js
@@ -5,14 +5,40 @@ var http = require('http');
 
 var app = express();
 
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 app.get('/keyboard', function (req, res) {
-    var data= {
-        'type' : 'buttons',
-        'buttons' : ['과일', '채소', '빵']
+    var data = {
+        'type': 'buttons',
+        'buttons': ['과일', '채소', '빵']
     };
 
     res.json(data);
 });
+
+
+
+app.post('/message', function (req, res) {
+    var msg = req.body.content;
+
+    console.log('전달메시지 : ' + msg);
+
+
+
+    var send = {
+        'message': {
+            'text' : 'sdfs'
+        }
+    };
+
+
+})
+
+
+
 
 http.createServer(app).listen(process.env.PORT || 9000, function () {
     console.log('서버 실행')

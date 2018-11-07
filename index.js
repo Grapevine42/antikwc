de6ce5b07bf268d6995945376e915368d5112a57
grapevine <jinsw2001@naver.com>
2018-11-08 02:08:20 +0900

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+var http = require('http');
+
+
+var app = express();
+
+app.get('/keyboard', function (req, res) {
+    var data= {
+        'type' : 'buttons',
+        'buttons' : ['과일', '채소', '빵']
+    };
+
+    res.json(data);
+});
+
+http.createServer(app).listen(9090, function () {
+    console.log('서버 실행')
+});
+
